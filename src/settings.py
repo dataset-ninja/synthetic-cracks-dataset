159510bc17c263e88592fc38f649ff71006ed483
Ars Vladimirov <grokhael@gmail.com>
2023-08-18 19:53:10 +0600

    upd

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Supervisely Synthetic Crack Segmentation"
 PROJECT_NAME_FULL: str = "Supervisely Synthetic Crack Segmentation"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
